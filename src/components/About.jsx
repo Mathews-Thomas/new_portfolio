25e411545698cc9949d4863faf23be00a5eb2ada
Mathews Thomas <mathewsxo@gmail.com>
2024-07-30 18:38:45 +0530

    new image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import aboutImage from '../assets/img/about.jpg';
+import aboutImage from '../assets/img/about.png';
 import { motion } from 'framer-motion';
 
 const About = () => {
